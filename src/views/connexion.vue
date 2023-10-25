be75a20ee77d1429d3fac275229003eede1b3842
Chloé Bizet <chloe.bizet@edu.univ-fcomte.fr>
2023-10-25 17:06:01 +0200

    connexion

--- src/views/connexion.vue
+++ src/views/connexion.vue
@@ -1,49 +1,51 @@
 <script setup>
-import { RouterLink } from "vue-router";
+import { RouterLink, routerKey, useRouter } from "vue-router";
 import headerVue from "../components/header.vue";
 import cardCommande from "../components/card-commande.vue";
 import footerVue from "../components/footer.vue";
+import { ref } from "vue";
+const router = useRouter();
 
 // Import pocketbase
 import PocketBase from "pocketbase";
 // Objet pocketBase
 var pocketbase_ip = "";
-if (import.meta.env.MODE === "production")
+/*if (import.meta.env.MODE === "production")
   pocketbase_ip = "http://193.168.146.127/:80";
-else pocketbase_ip = "http://127.0.0.1:8090";
+else pocketbase_ip = "http://127.0.0.1:8090";*/
+const pb = new PocketBase("http://127.0.0.1:8090");
+// user connecté ? au départ faux
+let isConnected = ref(false);
 
-  // user connecté ? au départ faux
-  let isConnected = ref(false)
+// Element de connexion
+let user = ref("");
+let psw = ref("");
 
-  // Element de connexion
-  let user = ref('')
-  let psw = ref('')
+// User connecté
+let currentUser = ref(null);
+let avatar = ref(null);
 
-  // User connecté
-  let currentUser = ref(null)
-  let avatar = ref(null)
-
-  const login=async()=>{
-  try{
-    const authData = await pb.collection('users')
-    .authWithPassword(user.value, psw.value)
-    console.log("connecté : ",authData)
-    //refresh()    
-  }catch(error){
-//    console.log("erreur de connexion : ",error.message)
-    alert("Erreur d'identification : mauvais login et/ou mot de passe")
-    user.value = ""
-    psw.value = ""
+const login = async () => {
+  try {
+    const authData = await pb
+      .collection("users")
+      .authWithPassword(user.value, psw.value);
+    console.log("connecté : ", authData);
+    //refresh()
+    router.push({ name: "index" });
+  } catch (error) {
+    console.log("erreur de connexion : ", error.message);
+    alert("Erreur d'identification : mauvais login et/ou mot de passe");
+    user.value = "";
+    psw.value = "";
   }
-}
-  }
-
+};
 </script>
 
 <template>
   <headerVue />
 
-  <h1 class="text-center mt-5 text-[25px]">Se connecter</h1>
+  <h1 class="text-center mt-5 text-[25px]">Se connecter xx</h1>
   <div class="grid justify-center space-y-12 mt-5">
     <input type="email" required id="email" v-model="user" />
     <input type="password" reqiured id="password" v-model="psw" />
@@ -51,7 +53,7 @@ else pocketbase_ip = "http://127.0.0.1:8090";
       <input type="checkbox" required id="checkbox" />
       <label class="ml-2">J'accepte les conditions d'utilisation</label>
     </div>
-    <button v-on:click="login()">Se connecter</button>
+    <button @:click="login()">Se connecter</button>
   </div>
   <footerVue />
 </template>
